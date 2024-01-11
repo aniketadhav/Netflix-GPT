04266d1e4f7dcb5f1e92546979f40030b5fe57e3
anketadhav <aniketadhav1997@gamil.com>
2024-01-11 09:40:42 +0530

    login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validation";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
-  
-  const handleButtonClick=() => {
+  const [errorMsg, seterrorMsg] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullName = useRef(null);
+
+  const handleButtonClick = () => {
     //Validate the From data;
-    
-  }
-  
+    const msg = checkValidData(email?.current?.value, password?.current?.value, fullName?.current?.value);
+    seterrorMsg(msg);
+    console.log("msg", msg);
+  };
+
   return (
     <div>
       <Header />
@@ -21,7 +28,10 @@ const Login = () => {
           alt="Background"
         />
       </div>
-      <form className="w-3/12 absolute p-12 text-white bg-black mt-36 mx-auto right-0 left-0 rounded bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 text-white bg-black mt-36 mx-auto right-0 left-0 rounded bg-opacity-80"
+      >
         <h1 className="font-bold text-white text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -29,6 +39,7 @@ const Login = () => {
           <input
             type="text"
             name=""
+            ref={fullName}
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-600"
           />
@@ -36,16 +47,18 @@ const Login = () => {
         <input
           type="text"
           name=""
+          ref={email}
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-600 "
         />
         <input
+          ref={password}
           type="password"
           name=""
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-600"
         />
-
+        <p className="text-red-500 font-bold text-lg py-2"> {errorMsg}</p>
         <button
           type="submit"
           className="p-4 my-4 bg-red-700 w-full rounded"
